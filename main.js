c3e3bceb9b70c8d94065b1074c2fd6cfb7a0a4c3
Codrigo Campos <roantocampos@gmail.com>
2025-03-06 11:02:10 -0600

    colorsitos

--- main.js
+++ main.js
@@ -11,26 +11,32 @@ function addSong(songName, artistName, songUrl){
             - ${artistName}
             <a href="${songUrl}" target="_blank" >Reproducir</a>
             <Button class="delete-btn"> Eliminar </Button>
-    
+   
     `
     //agregar al HTML
     playList.appendChild(listSong);
+ 
+    //agregar evento al boton de eliminar
+const deleteButton = listSong.getElementByIdBuCLassName ('delete.btn');
+deleteButton.addEventListener('click', () => {
+    listSong.remove(); //Eliminar la cancion de la lista
+})
 
-    
 }
-
+ 
 //Manejo de envio de formularios (Eventos)
-
+ 
 songForm.addEventListener('submit', (event) => {
     event.preventDefault();
-
+ 
     //Obtener datos del form
     const songName = document.getElementById("song_name").value;
     const artistName = document.getElementById("artist_name").value;
     const songUrl = document.getElementById("url_song").value;
-
+ 
     addSong(songName, artistName, songUrl);
-
+ 
     songForm.reset();
-
+ 
 })
+ 
